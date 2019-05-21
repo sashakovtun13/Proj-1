a3c4eb74d79a3b4824bc6d691722cf9a3b42d2df
Aleks <sashakovtun13@gmail.com>
2019-05-21 11:27:24 +0300

    Random 20 Kovtun

--- Program.cs
+++ Program.cs
@@ -8,6 +8,19 @@ namespace Proj
         {
             Console.WriteLine("Hi guys!");
             System.Console.WriteLine("Shehurin");
+ 		Random random = new Random();
+            List<int> list = new List<int>();
+            for (int i = 0; i <=20; i++)
+            {
+                list.Add(random.Next(1, 10));
+            }
+            foreach (var i in list)
+            {
+                if (i % 3 == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
