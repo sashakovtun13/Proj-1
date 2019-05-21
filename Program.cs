07d27e8208db8a408f35ef1be7ada0072a7b20f4
Aleks <sashakovtun13@gmail.com>
2019-05-21 11:41:30 +0300

    Kovtun

--- Program.cs
+++ Program.cs
@@ -6,20 +6,18 @@ namespace Proj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi guys!");
-            System.Console.WriteLine("Shehurin");
- 		Random random = new Random();
+		//Kovtun
+            Random random = new Random();
             List<int> list = new List<int>();
             for (int i = 0; i <=20; i++)
             {
                 list.Add(random.Next(1, 10));
             }
-            foreach (var i in list)
+            List<int> list2 = list.Where(x => x % 3 == 2).ToList();
+
+            foreach(var i in list2)
             {
-                if (i % 3 == 2)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(i);
             }
         }
     }
